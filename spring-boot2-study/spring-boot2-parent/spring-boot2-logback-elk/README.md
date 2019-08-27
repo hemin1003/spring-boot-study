375a9a27906b1d1e0da8c41195849da646f2d611
minbohe <hemin_it@163.com>
2019-08-27 13:59:12 +0800

    ELK日志集成

---
+++ spring-boot2-study/spring-boot2-parent/spring-boot2-logback-elk/README.md
@@ -0,0 +1,38 @@
+# spring-boot2-logback-elk
+
+【ELK日志集成】技术点
+
+功能介绍
+
+1. ELK日志集成说明
+
+## 本项目教程
+
+[ELK日志集成](https://hemin.blog.csdn.net/article/details/99637553)
+
+## 该系列教程
+
+[SpringBoot2系列](https://blog.csdn.net/hemin1003/column/info/40170)
+
+## 附加
+
+[从零开始搭建一个ELK实时日志收集系统](https://blog.csdn.net/hemin1003/article/details/73295303)
+
+## 个人说明
+
+期望和大家”一起学习，一起成长“，共勉，O(∩_∩)O谢谢
+
+不讲虚的，只做实干家
+
+Talk is cheap，show me the code
+
+<br/>
+
+
+## [关于我](http://heminit.com/about/)
+
+欢迎交流问题，可加我的个人QQ 469580884，或Q群号 751925591，一起探讨交流问题
+
+[我的博客地址](http://blog.csdn.net/hemin1003)
+
+[个人域名](http://heminit.com)
---
+++ spring-boot2-study/spring-boot2-parent/spring-boot2-logback-elk/src/main/java/com/md/demo/WebConfig.java
@@ -0,0 +1,34 @@
+package com.md.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 拦截器定义
+ * 
+ * @author Minbo.He
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+	// 让bean提前加载，让拦截器中的@Autowired生效
+	@Bean
+	public HandlerInterceptor getMyInterceptor() {
+		return new MyHttpInterceptor();
+	}
+
+	/**
+	 * 可定义多个拦截器
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// 定义过滤拦截的url名称，拦截所有请求
+		registry.addInterceptor(this.getMyInterceptor()).addPathPatterns("/**");
+//		registry.addInterceptor(其他拦截器).addPathPatterns("/**");
+		super.addInterceptors(registry);
+	}
+
+}
