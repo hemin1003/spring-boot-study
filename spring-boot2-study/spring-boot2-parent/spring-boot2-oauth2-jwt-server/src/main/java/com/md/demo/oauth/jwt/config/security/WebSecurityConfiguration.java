9f8dc639e54a90dae76f55323ed653c4300479ae
minbo <hemin_it@163.com>
2019-09-20 00:25:24 +0800

    Oauth2+JWT集成/SpringSecurity集成用法

--- spring-boot2-study/spring-boot2-parent/spring-boot2-oauth2-jwt-server/src/main/java/com/md/demo/oauth/jwt/config/security/WebSecurityConfiguration.java
+++ spring-boot2-study/spring-boot2-parent/spring-boot2-oauth2-jwt-server/src/main/java/com/md/demo/oauth/jwt/config/security/WebSecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -44,7 +45,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		if (passwordEncoder == null) {
-			passwordEncoder = DefaultPasswordEncoderFactories.createDelegatingPasswordEncoder();
+			passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		}
 		return passwordEncoder;
 	}
