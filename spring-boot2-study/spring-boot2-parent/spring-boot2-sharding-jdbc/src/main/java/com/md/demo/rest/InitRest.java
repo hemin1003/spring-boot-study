b1d9fb2fc6439289d1be9d353e96e67d22039bcb
minbo.he <hemin_it@163.com>
2019-10-21 16:49:10 +0800

    mybatis-plus快速开发框架用法

---
+++ spring-boot2-study/spring-boot2-parent/spring-boot2-sharding-jdbc/src/main/java/com/md/demo/rest/InitRest.java
@@ -0,0 +1,26 @@
+package com.md.demo.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Minbo
+ */
+@RestController
+public class InitRest {
+
+	protected static Logger logger = LoggerFactory.getLogger(InitRest.class);
+
+	/**
+	 * http://localhost:9090/hello
+	 * 
+	 * @return
+	 */
+	@GetMapping("/hello")
+	public String hello() {
+		logger.info("hello");
+		return "Hello greetings from spring-boot2-sharding-jdbc";
+	}
+}
